77adf6051f06da598be1f6557159134fa305d861
RoMcGill <ro.mcgill12@gmail.com>
2022-04-11 17:54:56 +0000

    define functions I will need,(may need to add some more or make changes)to create rough outline for project

--- run.py
+++ run.py
@@ -5,3 +5,27 @@ USER_BOARD = [[''] * 8 for i in range(8)]
 COMP_BOARD = [[''] * 8 for i in range(8)]
 USER_PLAY_BOARD = [[''] * 8 for i in range(8)]
 COMP_PLAY_BOARD = [[''] * 8 for i in range(8)]
+NAVIGATION = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
+
+def show_board(board):
+    pass
+
+def Place_ships(board):
+    pass
+
+def check_ship_size():
+    pass
+
+def ship_overlap():
+    pass
+
+def user_action():
+    pass
+
+def count_score():
+    pass
+
+def turn(board):
+    pass
+
+while True:
