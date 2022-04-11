1fe45f74798d4b04bc753fcbf65e9714428e7380
RoMcGill <ro.mcgill12@gmail.com>
2022-04-11 18:14:36 +0000

    create board, happy with how they look

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 import random
 
 SHIP_SIZES = [2,3,3,4,5]
-USER_BOARD = [[''] * 8 for i in range(8)]
-COMP_BOARD = [[''] * 8 for i in range(8)]
-USER_PLAY_BOARD = [[''] * 8 for i in range(8)]
-COMP_PLAY_BOARD = [[''] * 8 for i in range(8)]
+USER_BOARD = [[' '] * 8 for i in range(8)]
+COMP_BOARD = [[' '] * 8 for i in range(8)]
+USER_PLAY_BOARD = [[' '] * 8 for i in range(8)]
+COMP_PLAY_BOARD = [[' '] * 8 for i in range(8)]
 NAVIGATION = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
 
 def show_board(board):
-    pass
+    print('  a b c d e f g h')
+    print('  -+-+-+-+-+-+-+-+')
+    row_no = 1
+    #loop through row
+    for row in board:
+        # creating the grid formatting decimal, string and joining (|) to each row 
+        print('%d|%s|' % (row_no,'|'.join(row)))
+        row_no += 1
+    
+show_board(USER_BOARD)
 
 def Place_ships(board):
     pass
@@ -28,4 +37,4 @@ def count_score():
 def turn(board):
     pass
 
-while True:
+#while True:
