406d546a7f55f82aacddb6594e2c36199ad27447
RoMcGill <ro.mcgill12@gmail.com>
2022-04-11 18:38:57 +0000

    expand on functions 'place ships,check ship size, check ship overlap

--- run.py
+++ run.py
@@ -16,17 +16,46 @@ def show_board(board):
         # creating the grid formatting decimal, string and joining (|) to each row 
         print('%d|%s|' % (row_no,'|'.join(row)))
         row_no += 1
-    
-show_board(USER_BOARD)
 
-def Place_ships(board):
-    pass
+def Place_ships(USER_BOARD):
+    for ship_size in SHIP_SIZES:
+        while True:
+            if board == COMP_BOARD:
+                row, column, orientation = random.choice(['h', 'v']), random.randint(0, 7), random.randint(0, 7)
+                if check_ship_size(ship_size, row, column, orientation):
+                    if ship_overlap(ship_size, row, column, orientation) == False:
+                        #place ship
+                        if orientation == 'h':
+                            for i in range (column, column + ship_size):
+                                board[row][i] = 'x'
+                        else:
+                            for i in range(row, row + ship_size):
+                                board[i][column] = 'x'
+                        break
 
-def check_ship_size():
-    pass
+def check_ship_size(SHIP_SIZES, row, column, orientation):
+    if orientation == 'h':
+        if column + SHIP_SIZES > 8:
+            return False
+        else:
+            return True
+    else:
+        if row + SHIP_SIZES > 8:
+            return False
+        else:
+            return True
 
-def ship_overlap():
-    pass
+
+def ship_overlap(SHIP_SIZES, row, column, orientation):
+    if orientation == 'h':
+        for i in range(column, column + ship_size):
+            if board[row][i] == 'x':
+                return True
+    else:
+        for i in range(row,row + ship_size):
+            if board[i][column] == 'x':
+                return True
+    return False
 
 def user_action():
     pass
@@ -37,4 +66,4 @@ def count_score():
 def turn(board):
     pass
 
-#while True:
+while True:
