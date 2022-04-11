7d4bd248d918f144ba1d9ae61e84b7aeebf26b54
RoMcGill <ro.mcgill12@gmail.com>
2022-04-11 17:44:42 +0000

    add const variables for ship sizes and user and computers boards

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
 import random
 
+SHIP_SIZES = [2,3,3,4,5]
+USER_BOARD = [[''] * 8 for i in range(8)]
+COMP_BOARD = [[''] * 8 for i in range(8)]
+USER_PLAY_BOARD = [[''] * 8 for i in range(8)]
+COMP_PLAY_BOARD = [[''] * 8 for i in range(8)]
