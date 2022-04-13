15a494c5ce01a40bc826c1250a7a4e1faacf5d29
RoMcGill <ro.mcgill12@gmail.com>
2022-04-13 12:37:45 +0000

    install and import gspread and google oath2, define scope

--- run.py
+++ run.py
@@ -2,7 +2,20 @@
 import random
 from pyfiglet import figlet_format
 import os
-import keyboard
+# import keyboard
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('leaderboard')
 
 # constant variables
 SHIP_SIZES = [2, 3, 3, 4, 5]
