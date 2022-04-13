53f3af28c10e4ab4013f2f19f9527ea51d9ac0ca
RoMcGill <ro.mcgill12@gmail.com>
2022-04-13 11:45:44 +0000

    fixed bug with place ship stuck in loop, I acidentaly deleted half of my turn function it is fixed now

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ def Place_ships(board):
                 orientation, row, column = random.choice(['H', 'V']), random.randint(0, 7), random.randint(0, 7)
                 if check_ship_size(ship_size, row, column, orientation):
                     if ship_overlaps(board, row, column, orientation, ship_size,) == False:
-                        #place ship
+                        # place ship
                         if orientation == 'H':
                             for i in range(column, column + ship_size):
                                 board[row][i] = 'X'
@@ -87,7 +87,6 @@ def check_ship_size(SHIP_SIZE, row, column, orientation):
     """
     if orientation == "H":
         if column + SHIP_SIZE > 8:
-            print('ship does not fit')
             return False
         else:
             return True
@@ -105,12 +104,10 @@ def ship_overlaps(board, row, column, orientation, ship_size):
     if orientation == 'H':
         for i in range(column, column + ship_size):
             if board[row][i] == 'X':
-                print('WE HAVE ALREADY GOT A SHIP THERE SOLDIER')
                 return True
     else:
         for i in range(row, row + ship_size):
             if board[i][column] == 'X':
-                print('WE HAVE ALREADY GOT A SHIP THERE SOLDIER')
                 return True
 
     return False
@@ -141,7 +138,7 @@ def user_action(Place_ship):
                 print('please enter a number between 1 and 8')
         while True:
             try:
-                column = input(f'WHAT COLUMN DO YOU WANT THIS SHIP PLACED IN {player_name}\n')
+                column = input(f'WHAT COLUMN DO YOU WANT THIS SHIP PLACED IN {player_name}\n').upper()
                 if column in 'ABCDEFGH':
                     column = NAVIGATION[column]
                     break
@@ -180,6 +177,22 @@ def count_score(board):
     return count
 
 
+def clearConsole():
+    """
+    housekeeping, this clears the terminal
+    when called for a cleener look and feel.
+    """
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+
+
+
+username_input()
+
+
 def turn(board):
     """
     defines whos 'go' it is
@@ -208,28 +221,31 @@ def turn(board):
             board[row][column] = '-'
 
 
-def clearConsole():
-    """
-    housekeeping, this clears the terminal
-    when called for a cleener look and feel.
-    """
-    command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-    os.system(command)
+Place_ships(COMP_BOARD)
+show_board(COMP_BOARD)
+#clearConsole()
+show_board(USER_BOARD)
+Place_ships(USER_BOARD)
 
-
-def main():
-    """
-    main function to run all
-    functions in one place.
-    """
-  
-    Place_ships(COMP_BOARD)
-    #clearConsole()
-    #show_board(COMP_BOARD)
-    show_board(USER_BOARD)
-    Place_ships(USER_BOARD)
+while True:
+    #player turn
+    while True:
+        print('Guess a battleship location')
+        show_board(USER_PLAY_BOARD)
+        turn(USER_PLAY_BOARD)
+        break
+    if count_score(USER_PLAY_BOARD) == 17:
+        print(figlet_format("WINNER", font = "standard"))
+        print('YOU SAVED ALOT OF LIVES OUT THERE TODAY SOLDIER')
+        break   
+    #computer turn
+    while True:
+        turn(COMP_PLAY_BOARD)
+        break           
+    show_board(COMP_PLAY_BOARD)   
+    if count_score(COMP_PLAY_BOARD) == 17:
+        print("WE MAY HAVE LOST THE BATTLE, BUT NOT THE WAR!")
+        break
 
 
 def landing_page():
@@ -242,7 +258,7 @@ def landing_page():
     answer = input('ENTER Y OR N: ').upper()
     while True:
         if answer == "Y":
-            main()
+            turn(board)
         elif answer == "N":
             print('YOU WERE A GOOD SOLDIER!')
             return False
@@ -250,33 +266,4 @@ def landing_page():
             print('PLEASE ENTER Y OR N')
             answer = input('ENTER Y OR N: \n').upper()
 
-
-def leaderboard():
-    """
-    leaderboard, api to read and write to google sheet
-    """
-
-
-username_input()
 landing_page()
-
-
-# deciding the winner based on the data.
-while True:
-    #user turn
-    while True:
-        print(f'WHERE DO YOU WANT TO PUT THESE SHIPS {player_name}\n'.upper())
-        show_board(USER_PLAY_BOARD)
-        turn(USER_PLAY_BOARD)
-        break
-    if count_score(USER_PLAY_BOARD) == 17:
-        print(f'YOU DID IT {player_name}\n'.upper())
-        break
-    #comp turn
-    while True:
-        turn(COMP_PLAY_BOARD)
-        break
-    show_board(COMP_PLAY_BOARD)
-    if count_score(COMP_PLAY_BOARD) == 17:
-        print(f'TODAY WAS NOT YOUR FINEST OFFICER {player_name}\n'.upper())
-        break
